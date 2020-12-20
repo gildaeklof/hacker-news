d07bf36515b4ee809d414c0dd906f38b8bb4e364
Gilda Eklof <gilda@eklof.se>
2020-12-20 17:03:04 +0100

    startup files

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
-<?php require __DIR__ . '/header.php'; ?>
+<?php require __DIR__ . '/views/header.php'; ?>
 
 
 
-<?php require __DIR__ . '/footer.php'; ?>
+<?php require __DIR__ . '/views/footer.php'; ?>
